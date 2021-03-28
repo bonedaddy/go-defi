c738db9ba0d2c60ea7c67b19ad369c3ab41e2100
bonedaddy <boned4ddy@protonmail.com>
2021-03-27 17:15:28 -0700

    wip

--- uniswap/v3/client.go
+++ uniswap/v3/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	v3factory "github.com/bonedaddy/go-defi/bindings/uniswap/v3/factory"
+	v3nftposmanager "github.com/bonedaddy/go-defi/bindings/uniswap/v3/nftposmanager"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/bonedaddy/go-defi/utils"
@@ -29,4 +31,28 @@ func (c *Client) Close() {
 // Factory returns a uniswapv3 factory contract binding
 func (c *Client) Factory(addr string) (*v3factory.V3factory, error) {
 	return v3factory.NewV3factory(common.HexToAddress(addr), c.bc)
+}
+
+// DeployNFTPositionManager deploys an pool nft position manager
+func (c *Client) DeployNFTPositionManager(
+	auth *bind.TransactOpts,
+	factory,
+	weth9,
+	tokenDescriptor common.Address,
+) (*v3nftposmanager.V3nftposmanager, common.Address, error) {
+	addr, tx, contract, err := v3nftposmanager.DeployV3nftposmanager(
+		auth,
+		c.bc,
+		factory,
+		weth9,
+		tokenDescriptor,
+	)
+	if err != nil {
+		return nil, common.Address{}, err
+	}
+	_, err = bind.WaitDeployed(c.ctx, c.bc, tx)
+	if err != nil {
+		return nil, common.Address{}, err
+	}
+	return contract, addr, nil
 }
